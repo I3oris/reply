26f325e1d4bf9962eb08a6a3b515946845f2a62c
I3oris <boris31.resch@gmail.com>
2022-10-06 23:05:11 +0200

    Make `ioctl` fun more portable (Use `LibC::Int` instead of `Int32`, etc..).

--- src/term_size.cr
+++ src/term_size.cr
@@ -1,21 +1,21 @@
+lib LibC
+  struct Winsize
+    row : LibC::Short
+    col : LibC::Short
+    x_pixel : LibC::Short
+    y_pixel : LibC::Short
+  end
+
+  TIOCGWINSZ = 0x5413 # Magic number.
+
+  fun ioctl(fd : LibC::Int, request : LibC::SizeT, winsize : LibC::Winsize*) : LibC::Int
+end
+
 module Reply::Term
   module Size
-    lib C
-      struct WinSize
-        row : UInt16
-        col : UInt16
-        x_pixel : UInt16
-        y_pixel : UInt16
-      end
-
-      TIOCGWINSZ = 21523 # Magic number.
-
-      fun ioctl(fd : Int32, request : UInt32, winsize : C::WinSize*) : Int32
-    end
-
     # Gets the terminals width
     def self.size : {Int32, Int32}
-      ret = C.ioctl(1, C::TIOCGWINSZ, out screen_size)
+      ret = LibC.ioctl(1, LibC::TIOCGWINSZ, out screen_size)
       raise "Error retrieving terminal size: ioctl TIOCGWINSZ: #{Errno.value}" if ret < 0
 
       {screen_size.col.to_i32, screen_size.row.to_i32}
